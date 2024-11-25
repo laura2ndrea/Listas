ca14f14c884596824d0c79caf21b1fac699431ff
Laura Andrea Rodríguez Mejía <91057900+laura2ndrea@users.noreply.github.com>
2024-11-25 13:23:29 -0500

    Add files via upload

---
+++ Listas/src/main/java/campuslands/listas/Listas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package campuslands.listas;
+
+/**
+ *
+ * @author camper
+ */
+public class Listas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
