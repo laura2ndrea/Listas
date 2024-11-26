292a15e86ef42a418388489692be8f25b5635235
Laura Andrea Rodríguez Mejía <91057900+laura2ndrea@users.noreply.github.com>
2024-11-26 10:03:25 -0500

    Update ListaDoble.java

--- Listas/src/main/java/campuslands/listas/doblemente/ListaDoble.java
+++ Listas/src/main/java/campuslands/listas/doblemente/ListaDoble.java
@@ -9,5 +9,44 @@ package campuslands.listas.doblemente;
  * @author camper
  */
 public class ListaDoble {
+    private Nodo<T> cabeza; 
+    private int tamano; 
     
+    public ListaDoble() {
+        this.cabeza = null; 
+        this.tamano = 0; 
+    }
+    
+    public void mostrarTamano(){
+        System.out.println(tamano);
+    }
+    
+    public void mostrarOrden(){
+        Nodo <T> actual = cabeza; 
+        while (actual != null) {
+            System.out.print(actual.getDato() + " ");
+            actual = actual.getSiguiente(); 
+        }
+        System.out.println();
+    }
+    
+    public void mostrarInverso(){
+        Nodo <T> actual = cabeza; 
+        while (actual != null && actual.getSiguiente() != null){
+            actual = actual.getSiguiente(); 
+        }
+        
+        
+        
+    }
+    
+    public void agregarInicio(T dato){
+        Nodo<T> nuevoNodo = new Nodo<>(dato);
+        if (cabeza != null){
+            cabeza.setAnterior(nuevoNodo);
+            nuevoNodo.setSiguiente(cabeza);
+        }
+        cabeza = nuevoNodo; 
+        tamano +=1; 
+    }
 }
