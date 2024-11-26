635dec6c1a65aaff8db168c9e9ab50700fe148e2
Laura Andrea Rodríguez Mejía <91057900+laura2ndrea@users.noreply.github.com>
2024-11-26 10:04:18 -0500

    Update Nodo.java

--- Listas/src/main/java/campuslands/listas/doblemente/Nodo.java
+++ Listas/src/main/java/campuslands/listas/doblemente/Nodo.java
@@ -8,6 +8,38 @@ package campuslands.listas.doblemente;
  *
  * @author camper
  */
-public class Nodo {
+public class Nodo<T> {
+    T dato; 
+    Nodo<T> siguiente; 
+    Nodo<T> anterior; 
     
+    public Nodo(T dato){
+        this.dato = dato; 
+        this.siguiente = null; 
+        this.anterior = null; 
+    }
+    
+    public T getDato() {
+        return dato;
+    }
+    
+    public void setDato (T dato){
+        this.dato = dato; 
+    }
+    
+    public Nodo<T> getSiguiente(){
+        return siguiente; 
+    }
+    
+    public void setSiguiente (Nodo<T> siguiente){
+        this.siguiente = siguiente; 
+    }
+    
+    public Nodo<T> getAnterior(){
+        return anterior; 
+    }
+    
+    public void setAnterior (Nodo<T> anterior){
+        this.anterior = anterior; 
+    }
 }
